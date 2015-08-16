47a7fe9ce72cc9a7311fe266952037327055e4b8
Fabio Mello <fabiohmello@gmail.com>
2015-08-16 15:21:00 -0300

    Move some utility functions around.

--- utils.sh
+++ utils.sh
@@ -1,6 +1,44 @@
 
 utils_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+EXIT_SUCCESS="$(true && echo $?)"
+EXIT_FAILURE="$(false || echo $?)"
+
+confirm() {
+  echo "$@ (yes/no)?"
+  read response
+  if [ "${response}" != "yes" -a "${response}" != "no" ]; then
+    error 'The response should be "yes" or "no"'
+  elif [ "${response}" = "no" ]; then
+    return ${EXIT_FAILURE}
+  else
+    return ${EXIT_SUCCESS}
+  fi
+}
+
+depends_check() {
+  for depend in $1; do
+    if [ $(grep "${depend}" "$0" | wc -l) -lt 2 ]; then
+      echo -e "\e[33mINFO\e[m ${depend} is not a dependency" >&2
+    elif ! which ${depend} &> /dev/null; then
+      error "This script depens on ${depend}" >&2
+    fi
+  done
+}
+
+error() {
+  echo -e "\e[31mERROR\e[m $@" >&2
+  exit ${EXIT_FAILURE}
+}
+
+firstrun_config() {
+  if [ -f "${1:-}" ]; then
+    cat $1
+  else
+    ${utils_dir}/firstrun/generate
+  fi
+}
+
 platform_list_helper() {
   for platform in ${utils_dir}/firstrun/*; do
     [ ! -d "${platform}" ] && continue
@@ -13,14 +51,6 @@ platform_list_helper() {
 platform_list() {
   local platforms="$(platform_list_helper)"
   echo "${platforms:1}"
-}
-
-firstrun_config() {
-  if [ -f "${1:-}" ]; then
-    cat $1
-  else
-    ${utils_dir}/firstrun/generate
-  fi
 }
 
 # vim: ts=2 sw=2 sts=2 expandtab smartindent smarttab
